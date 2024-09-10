fa7c0cbf9ba6ed6249fd9fc2a3b05d51af28d8f0
AxelSV50 <axelsv50@gmail.com>
2024-09-09 21:03:37 -0600

    Iniciializando el proyecto

---
+++ tarea-spring-boot/tarea1_B97683/src/main/java/ucr/ac/tarea1_B97683/Tarea1B97683Application.java
@@ -0,0 +1,13 @@
+package ucr.ac.tarea1_B97683;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea1B97683Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea1B97683Application.class, args);
+	}
+
+}
---
+++ tarea-spring-boot/tarea1_B97683/src/test/java/ucr/ac/tarea1_B97683/Tarea1B97683ApplicationTests.java
@@ -0,0 +1,13 @@
+package ucr.ac.tarea1_B97683;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea1B97683ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
