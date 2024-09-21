a1d238cca46b2ab41b1ce9b9572d63c389841d4c
Axel Solís <axelsv50@gmail.com>
2024-09-20 19:40:30 -0600

    Config Eslint, Prettier e inicio de ejercicio React + TS

--- lab04-react/src/App.tsx
+++ lab04-react/src/App.tsx
@@ -1,13 +1,29 @@
-//import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
-  //const [count, setCount] = useState(0)
+	const [name, setName] = useState('Welcome to React');
+	const [names, setNames] = useState<string[]>([]);
 
-  return (
-    <>
-      Hello World!
-    </>
-  )
+  const addName = () =>{
+
+    setNames([...names, name])
+    //setName("")
+
+  }
+	return (
+		<>
+			<div style={{ textAlign: 'center', flexFlow: 'column', display: 'flex' }}>
+				<ul>
+          {names.map((name, index) => <li key = {index}>{name}</li>)}
+        </ul>
+				<input
+					type='text'
+					onChange={e => setName(e.target.value)}
+				/>
+        <button onClick={addName}>Agregar</button>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
