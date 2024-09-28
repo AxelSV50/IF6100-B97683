e638a0ae79d0712306f24968ed3559f78522ce4a
Axel Solís <axelsv50@gmail.com>
2024-09-27 19:37:17 -0600

    Laboratorio 6 - React + TS + Ant Design

---
+++ lab04-react/src/Details.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+interface DetailsProps {
+	names: string[];
+	title: string | undefined;
+}
+const Details = (props: DetailsProps) => {
+	const { names } = props;
+
+	return (
+		<div style={{ textAlign: 'center', display: 'flex', flexFlow: 'column' }}>
+			<h1>Details</h1>
+			<ol style={{ textAlign: 'center', alignSelf: 'center' }}>
+				{names.map((name, index) => (
+					<li key={index}>{name}</li>
+				))}
+			</ol>
+		</div>
+	);
+};
+
+export default Details;
